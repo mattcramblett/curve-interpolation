fd310f8e1e1f71f378a8da42e44157aff41156cf
matig123 <matiasgrotewold11@gmail.com>
2016-10-30 20:34:52 -0400

    different implementation options. currently sinusoidal

--- Assets/CatmullRomCurveInterpolation.cs
+++ Assets/CatmullRomCurveInterpolation.cs
@@ -107,12 +107,14 @@ public class CatmullRomCurveInterpolation : MonoBehaviour {
 			for(float u = 0.0f; u < 1.000f; u = u + DT){
 				Vector2 value = new Vector2 (n, u);
 				Vector3 newPoint = ComputePointOnCatmullRomCurve(u, n + 2);
-				//tempDistance is s
+				//original implementation
 				float tempDistance = Vector3.Distance (temporaryPoint, newPoint);
+				//single cubic ease-in
 				tempDistance = -2 * Mathf.Pow (u, 3) + 3 * Mathf.Pow (u, 2);
+				//sinusoidal ease-in
 				tempDistance = (Mathf.Sin (Mathf.PI * u - Mathf.PI / 2) + 1) / 2;
 				totalDistance += tempDistance;
-				if (Mathf.Abs(totalDistance - prevDistance) < .001 ) {
+				if (Mathf.Abs(totalDistance - prevDistance) < .0001 ) {
 					//tempMap.Add (totalDistance + .0001f, value);
 					//temporaryPoint = newPoint;
 				} else {
